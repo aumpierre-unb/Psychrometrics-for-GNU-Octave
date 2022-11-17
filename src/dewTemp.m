093c5e1d3a27c95884c9ad44feee09dd0e466ab9
aumpierre-unb <aumpierre@gmail.com>
2022-11-17 10:36:15 -0300

    major changes

--- src/dewTemp.m
+++ src/dewTemp.m
@@ -18,7 +18,7 @@
 # It is also available at https://www.gnu.org/licenses/.
 
 function Tdew=dewTemp(pw)
-    c=load("constants.dat");
+    run("constants.m");
     alpha=log(pw/1000);
     Tdew=c(14)+...
          c(15)*alpha+...
--- src/satPress.m
+++ src/satPress.m
@@ -18,7 +18,7 @@
 # It is also available at https://www.gnu.org/licenses/.
 
 function psat=satPress(Tdry)
-    c=load("constants.dat");
+    run("constants.m");
     if -100<=Tdry-273.15 & Tdry-273.15<0
         k=c(1)/Tdry+...
         c(2)+...
